20aeadf2e04dd96ccf1653648d7420d11adfb484
Yingyun Liu <YingyunLiu@yahoo.com>
2014-11-09 05:28:51 -0500

    project 1 code and figure files

--- plot1.R
+++ plot1.R
@@ -1,10 +1,17 @@
 
-a=read.table("household_power_consumption.txt", header=TRUE, na.strings="?", sep=";", as.is=TRUE)
+a=read.table("household_power_consumption.txt", 
+             header=TRUE, 
+             na.strings="?", 
+             sep=";", 
+             as.is=TRUE)
 a=a[a[,1]=="2/2/2007" | a[,1]=="1/2/2007", ] #subset to these rows
 datetime <- paste(a[,1], a[,2])
 datetime <- strptime(datetime, "%d/%m/%Y %H:%M:%S")
 a <- cbind(datetime, a[,3:9])
 
 png(filename="plot1.png")
-hist(a[,2], col="red", main="Global Active Power", xlab="Global Active Powerr (kilowatts)")
+hist(a[,2], 
+     col="red", 
+     main="Global Active Power", 
+     xlab="Global Active Powerr (kilowatts)")
 dev.off()
--- plot2.R
+++ plot2.R
@@ -1,10 +1,17 @@
 
-a=read.table("household_power_consumption.txt", header=TRUE, na.strings="?", sep=";", as.is=TRUE)
+a=read.table("household_power_consumption.txt", 
+             header=TRUE, 
+             na.strings="?", 
+             sep=";", 
+             as.is=TRUE)
 a=a[a[,1]=="2/2/2007" | a[,1]=="1/2/2007", ] #subset to these rows
 datetime <- paste(a[,1], a[,2])
 datetime <- strptime(datetime, "%d/%m/%Y %H:%M:%S")
 a <- cbind(datetime, a[,3:9])
 
 png(filename="plot2.png")
-plot(a[,1], a[,2], type="l", xlab="", ylab="Global Active Power (kilowatts)")
+plot(a[,1], a[,2], 
+     type="l", 
+     xlab="", 
+     ylab="Global Active Power (kilowatts)")
 dev.off()
